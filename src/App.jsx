9b583210301a5e8314b6ba2f0e565c806f047fc8
Natallia-Karatava <nkaratova@gmail.com>
2025-03-19 11:53:05 +0100

    hero+nav

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Header />
-     <Navigation />
     </div>
-    );
+  );
 }
 
 export default App;
