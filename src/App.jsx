e9af9d1b1b76e46484ca68d91c064d105db09514
Natallia-Karatava <nkaratova@gmail.com>
2025-03-19 13:37:16 +0100

    update index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-
-
-
 import React from "react";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import RollingGallery from "./components/RollingGallery";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
 function App() {
   return (
-
     <div className="App">
       <Navigation />
       <Header />
-    <RollingGallery autoplay={true} pauseOnHover={true} />
-    </div>
+      <RollingGallery autoplay={true} pauseOnHover={true} />
 
+      <Footer />
+    </div>
   );
 }
 
