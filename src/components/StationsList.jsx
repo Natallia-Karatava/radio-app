0a74c45377dedbb882fa7e6da4d59482415d237d
marrozhkova <mar.rozhkova@gmail.com>
2025-03-20 16:57:12 +0100

    share+like

--- src/components/StationsList.jsx
+++ src/components/StationsList.jsx
@@ -3,6 +3,7 @@ import { FetchContext } from "../contexts/FetchContext";
 import { useTranslation } from "react-i18next";
 import img from "../images/logos/SoundPulse_signet.png";
 import "../styles/StationsList.css";
+import { MdDelete } from "react-icons/md";
 
 const StationsList = () => {
   const { t } = useTranslation();
@@ -19,6 +20,8 @@ const StationsList = () => {
     currentPage,
     setItemsPerPage,
     stationGenre,
+    handleStationClick,
+    deleteFavorite,
   } = useContext(FetchContext);
 
   useEffect(() => {
@@ -44,10 +47,17 @@ const StationsList = () => {
   }, [setItemsPerPage]);
 
   const stationsToDisplay = getStationsToDisplay();
-  const showPagination = displayMode === "all" && stationsToDisplay?.length > 0;
+  const isEmpty =
+    displayMode === "favorites" &&
+    (!stationsToDisplay || stationsToDisplay.length === 0);
+
+  const showPagination =
+    (displayMode === "all" || displayMode === "genre") &&
+    stationsToDisplay?.length > 0 &&
+    !isEmpty;
 
   return (
-    <div className="stations-container">
+    <div className={`stations-container ${isEmpty ? "empty-favorites" : ""}`}>
       <h2 className="h2">
         {displayMode === "genre" && stationGenre
           ? `${t("Radio Stations")} - ${t(stationGenre)}`
@@ -55,61 +65,80 @@ const StationsList = () => {
           ? t("My Favorites")
           : t("Radio Stations")}
       </h2>
-      {isLoading && <div className="loading">{t("Loading...")}</div>}
-      <div className="stations-list">
-        {stationsToDisplay?.map((station) => (
-          <div
-            key={station.id}
-            className={`station-item ${
-              currentStation?.id === station.id ? "active" : ""
-            }`}
-            onClick={() => handleStationClick(station)}
-          >
-            <div className="station-logo">
-              <img src={station.favicon || img} alt="radiostation logo" />
-            </div>
-            <div className="station-description">
-              <p className="text-m">{station.name}</p>
-              <p className="text-xs">
-                {station.country}:{" "}
-                {Array.isArray(station.tags)
-                  ? station.tags.join(", ")
-                  : typeof station.tags === "string"
-                  ? station.tags
-                      .split(/(?=[A-Z])/)
-                      .join(", ")
-                      .toLowerCase()
-                  : station.tags}
-              </p>
-              <p className="text-xs">
-                {station.codec} • {station.bitrate}kbps
-              </p>
-            </div>
-          </div>
-        ))}
-      </div>
 
-      {showPagination && (
-        <div className="pagination-controls">
-          {currentPage > 0 && (
-            <button
-              className="button button-next-prev"
-              onClick={previousPage}
-              disabled={isLoading}
-            >
-              {t("Previous")} {currentPage}
-            </button>
+      {isEmpty ? (
+        <div className="empty-message">{t("No favorites yet")}</div>
+      ) : (
+        <>
+          {isLoading && <div className="loading">{t("Loading...")}</div>}
+          <div className="stations-list">
+            {stationsToDisplay?.map((station) => (
+              <div
+                key={station.id}
+                className={`station-item ${
+                  currentStation?.id === station.id ? "active" : ""
+                }`}
+                onClick={() => handleStationClick(station)}
+              >
+                {" "}
+                {displayMode === "favorites" && (
+                  <button
+                    className="delete-button"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      deleteFavorite(station.id);
+                    }}
+                  >
+                    <MdDelete size={24} />
+                  </button>
+                )}
+                <div className="station-logo">
+                  <img src={station.favicon || img} alt="radiostation logo" />
+                </div>
+                <div className="station-description">
+                  <p className="text-m">{station.name}</p>
+                  <p className="text-xs">
+                    {station.country}:{" "}
+                    {Array.isArray(station.tags)
+                      ? station.tags.join(", ")
+                      : typeof station.tags === "string"
+                      ? station.tags
+                          .split(/(?=[A-Z])/)
+                          .join(", ")
+                          .toLowerCase()
+                      : station.tags}
+                  </p>
+                  <p className="text-xs">
+                    {station.codec} • {station.bitrate}kbps
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {showPagination && (
+            <div className="pagination-controls">
+              {currentPage > 0 && (
+                <button
+                  className="button button-next-prev"
+                  onClick={previousPage}
+                  disabled={isLoading}
+                >
+                  {t("Previous")} {currentPage}
+                </button>
+              )}
+              {hasMore && (
+                <button
+                  className="button button-next-prev"
+                  onClick={nextPage}
+                  disabled={isLoading}
+                >
+                  {t("Next")} {currentPage + 2}
+                </button>
+              )}
+            </div>
           )}
-          {hasMore && (
-            <button
-              className="button button-next-prev"
-              onClick={nextPage}
-              disabled={isLoading}
-            >
-              {t("Next")} {currentPage + 2}
-            </button>
-          )}
-        </div>
+        </>
       )}
     </div>
   );
