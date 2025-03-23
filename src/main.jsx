858b2d12238d5352254d6ee2cedc02994de761af
marrozhkova <mar.rozhkova@gmail.com>
2025-03-23 09:53:26 +0100

    dark-mode+share

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import App from "./App.jsx";
 import { FetchProvider } from "./contexts/FetchContext.jsx";
 import { I18nextProvider } from "react-i18next";
 import TranslateContext from "./contexts/TranslateContext.jsx";
+import { ThemeProvider } from "./contexts/ThemeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <I18nextProvider i18n={TranslateContext}>
-      <FetchProvider>
-        <App />
-      </FetchProvider>
+      <ThemeProvider>
+        <FetchProvider>
+          <App />
+        </FetchProvider>
+      </ThemeProvider>
     </I18nextProvider>
   </StrictMode>
 );
